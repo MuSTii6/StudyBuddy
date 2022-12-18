39463014b458d07ce37af6484645e0a4e89f3140
uzdilmus <uzdilmus@students.zhaw.ch>
2022-12-18 11:42:27 +0100

    created a "WelcomeController" for test

---
+++ src/main/java/studybuddy/studybuddy/controller/WelcomeController.java
@@ -0,0 +1,17 @@
+package studybuddy.studybuddy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+   
+    @GetMapping("/hello")
+public String helloWorld(@RequestParam final String name) {
+return "Hello " + name + "\n";
+
+}
+
+
+}
