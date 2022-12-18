dc269e9625c32fbee288ec79f794c97bd13e8dba
uzdilmus <uzdilmus@students.zhaw.ch>
2022-12-18 12:36:23 +0100

    created class Student and StudentCreateDTO

---
+++ src/main/java/studybuddy/studybuddy/model/StudentCreateDTO.java
@@ -0,0 +1,22 @@
+package studybuddy.studybuddy.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class StudentCreateDTO {
+    private String email;
+    private String name;
+
+
+
+
+
+
+
+
+
+
+
+}
